daa1efdfd5df146575223103ec6077d456c4e59c
Pandey <ashish.pandey@capgemini.com>
2020-04-20 08:42:20 +0530

    Updates

---
+++ precache-manifest.c7a00a5071f771edc0ff3981fa4ad8f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcdecd90f6bbac1123ddfcb833c5aee2",
+    "url": "/react_stopwatch/index.html"
+  },
+  {
+    "revision": "70b5185f96125dbcb47a",
+    "url": "/react_stopwatch/static/css/main.9b266e9d.chunk.css"
+  },
+  {
+    "revision": "3192f0082f0e67657f4e",
+    "url": "/react_stopwatch/static/js/2.145a5f61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_stopwatch/static/js/2.145a5f61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70b5185f96125dbcb47a",
+    "url": "/react_stopwatch/static/js/main.011f0011.chunk.js"
+  },
+  {
+    "revision": "869befaed09b77269b8b",
+    "url": "/react_stopwatch/static/js/runtime-main.a166661e.js"
+  }
+]);
